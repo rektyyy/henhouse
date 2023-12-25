07243208e7aaf554ac03e246cba34c6dac85ebe5
rektyyy <michik1324@gmail.com>
2023-12-25 20:39:33 +0100

    added tic tac toe (need to add rooms to it)

--- client/client.js
+++ client/client.js
@@ -1,22 +1,27 @@
 const socket = io();
-
 const form = document.getElementById('form');
 const input_message = document.getElementById('input_message');
 const input_room = document.getElementById('input_room');
 const button_room = document.getElementById('button_room');
 const messages = document.getElementById('messages');
+const board = document.getElementById('board');
+const cells = document.querySelectorAll('.cell');
 
 let currentRoomId = '0';
+let currentPlayer = 'X';
 
+
+// Sends message to server 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     if (input_message.value) {
-        socket.emit('chat message', {roomId: currentRoomId, message: input_message.value});
-        console.log({roomId: currentRoomId, message: input_message.value});
+        socket.emit('chat message', { roomId: currentRoomId, message: input_message.value });
+        console.log({ roomId: currentRoomId, message: input_message.value });
         input_message.value = '';
     }
 });
 
+// Sends join room to server
 button_room.addEventListener('click', () => {
     if (input_room.value) {
         currentRoomId = input_room.value;
@@ -27,15 +32,40 @@ button_room.addEventListener('click', () => {
 
 })
 
-socket.onAny((event, ...args) => {
-    console.log(event, args);
-});
-
-
+// Client recieves message
 socket.on('chat message', (msg) => {
     console.log(`Client recieved a message ${msg}`)
     const item = document.createElement('li');
     item.textContent = msg;
     messages.appendChild(item);
     window.scrollTo(0, document.body.scrollHeight);
+});
+
+// Tic tac toe game loop
+cells.forEach(cell => {
+    cell.addEventListener('click', () => {
+        const index = cell.dataset.index;
+        socket.emit('move', { index });
+    });
+});
+
+socket.on('updateGame', ({ boardState, winner }) => {
+    updateBoard(boardState);
+
+    if (winner) {
+        alert(`${winner} wygrywa!`);
+    } else if (!boardState.includes('')) {
+        alert('Remis!');
+    }
+});
+
+function updateBoard(boardState) {
+    cells.forEach((cell, index) => {
+        cell.textContent = boardState[index];
+    });
+}
+
+// logs events
+socket.onAny((event, ...args) => {
+    console.log(event, args);
 });
--- server/server.js
+++ server/server.js
@@ -11,6 +11,8 @@ const path = "/../client"
 
 app.use(express.static(__dirname + path));
 
+let boardState = Array(9).fill('');
+let currentPlayer = 'X';
 
 io.on('connection', (socket) => {
 
@@ -28,9 +30,45 @@ io.on('connection', (socket) => {
     console.log(`Server sending message to room ${data.roomId}: ${data.message}`)
     io.to(data.roomId).emit('chat message', data.message );
   });
+
+  socket.emit('updateGame', { boardState, winner: checkWinner() });
+
+  socket.on('move', ({ index }) => {
+    if (!boardState[index]) {
+      boardState[index] = currentPlayer;
+      io.emit('updateGame', { boardState, winner: checkWinner() });
+
+      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    } else {
+      // Gracz próbuje oszukać, nie zmieniaj stanu planszy
+      console.log('Niezgodność ruchu z zasadami gry!');
+    }
+  });
 });
+
+function checkWinner() {
+  const winningCombinations = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
+    [0, 4, 8], [2, 4, 6]             // diagonals
+  ];
+
+  for (const combo of winningCombinations) {
+    const [a, b, c] = combo;
+    if (boardState[a] && boardState[a] === boardState[b] && boardState[a] === boardState[c]) {
+      return boardState[a];
+    }
+  }
+
+  if (!boardState.includes('')) {
+    return 'Draw';
+  }
+
+  return null;
+}
 
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
 });
+
