f37775d486bd82e35583a27f89e3aa118662091a
Michał Wilczkowski <80486068+rektyyy@users.noreply.github.com>
2024-01-02 23:39:05 +0100

    Delete .history directory

--- .history/client/rooms_20231230144539.js
+++
@@ -1,6 +0,0 @@
-const socket = io();
-const button_get_rooms = document.getElementById('button_get_rooms')
-button_get_rooms.addEventListener('click',()=>{
-    socket.emit('request rooms');
-    window.location.href = '/rooms.html';
-})
